cac6b010e1724786293a13bf901506759695963b
Ricardo <diiaz2910@gmail.com>
2024-07-26 18:39:28 +1200

    ItemsList component deleted. Home page updated.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ItemList from "../components/ItemsList/ItemsList";
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <ItemList />
     </div>
   );
 };
