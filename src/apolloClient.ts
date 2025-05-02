b908d61856f198c9116d03a97e9ba8b397744050
Ricardo <diiaz2910@gmail.com>
2025-05-02 16:22:54 -0500

    Started development again, checked and set env.

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://lotto-dusky-one.vercel.app/api",
+  uri: "http://localhost:4500/api",
   cache: new InMemoryCache(),
 });
 export default client;
+
+// URI que apunta al al deployment de Vercel, cambiar cuando se termine el desarrollo
+// https://lotto-dusky-one.vercel.app/api
