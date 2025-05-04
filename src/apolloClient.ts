6be55b6b02ed98cc71495d7bc59a4bd926574786
Ricardo <diiaz2910@gmail.com>
2025-05-04 16:01:32 -0500

    Testing backend server

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,10 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4500/api",
+  uri: "https://lotto-dusky-one.vercel.app/api",
   cache: new InMemoryCache(),
 });
 export default client;
 
 // URI que apunta al al deployment de Vercel, cambiar cuando se termine el desarrollo
 // https://lotto-dusky-one.vercel.app/api
+// http://localhost:4500/api
